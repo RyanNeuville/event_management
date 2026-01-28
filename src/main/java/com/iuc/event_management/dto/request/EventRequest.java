29ec849e6092e8b2cb35ac823fc0ac55eb3e38e7
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-28 02:43:45 +0100

    refactor: Enhance DTOs with constructors and accessors, and clean up the Role entity by removing commented code.

--- src/main/java/com/iuc/event_management/dto/request/EventRequest.java
+++ src/main/java/com/iuc/event_management/dto/request/EventRequest.java
@@ -6,4 +6,55 @@ public class EventRequest {
     public LocalDate date;
     public String location;
     public String image;
+
+    public EventRequest() {
+    }
+
+    public EventRequest(String title, String description, LocalDate date, String location, String image) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- src/main/java/com/iuc/event_management/dto/response/ReservationResponse.java
+++ src/main/java/com/iuc/event_management/dto/response/ReservationResponse.java
@@ -5,4 +5,46 @@ public class ReservationResponse {
     public LocalDate date;
     public Boolean status;
     public Long eventId;
+
+    public ReservationResponse() {
+    }
+
+    public ReservationResponse(Long id, LocalDate date, Boolean status, Long eventId) {
+        this.id = id;
+        this.date = date;
+        this.status = status;
+        this.eventId = eventId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
 }
